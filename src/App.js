3f23fb497f67c2b637aadeb4bf07dfad1c6b7d16
Katerin <ckaterinarellano@gmail.com>
2023-05-24 19:10:10 -0500

    feat: add dynamic form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Button from 'react-bootstrap/Button';
 import NavbarMenu from './components/NavbarMenu';
+import DynamicForm from './components/DynamicForm/DynamicForm';
 
 function App() {
   return (
-    <NavbarMenu/>
+    <>
+      <NavbarMenu/>
+      <DynamicForm/>
+    </>
   );
 }
 
